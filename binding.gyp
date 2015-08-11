eee2b4afdf5ab2ffff36f7c0050e92722bb337a1
Samo Fabčič <samo.fabcic@gmail.com>
2015-08-11 20:47:08 +0200

    added visa code; think of how to squeeze that callback signature to my object..

--- binding.gyp
+++ binding.gyp
@@ -11,6 +11,23 @@
 	{
         "target_name" : "makecallback"
       , "sources"     : [ "cpp/makecallback.cpp" ]
+      , 'include_dirs': [
+              'C:\Program Files\IVI Foundation\VISA\Win64\Include'
+            ]
+      , 'link_settings': {
+				  'library_dirs': [
+                  'C:\Program Files\IVI Foundation\VISA\Win64\Lib_x64\msc',
+                ],
+          'libraries': [
+                  'visa64',
+                ],
+              }
+      ,'msvs_settings': {
+              'VCCLCompilerTool': {
+                'ExceptionHandling': '2',
+                'DisableSpecificWarnings': [ '4530', '4506' ],
+              },
+       }
     }
    ]
 }
--- cpp/makecallback.cpp
+++ cpp/makecallback.cpp
@@ -1,5 +1,20 @@
 #include <nan.h>
 
+#include <visa.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+
+#include <string.h>
+#include <ctime>
+
+#define MAX_CNT 1024
+
+ViStatus write(ViSession instr1, const char* input);
+ViStatus _VI_FUNCH callback(ViSession vi, ViEventType etype, ViEvent eventContext, ViAddr userHandle);
+
 using namespace Nan;  // NOLINT(build/namespaces)
 
 class MyObject : public node::ObjectWrap {
@@ -9,10 +24,11 @@ class MyObject : public node::ObjectWrap {
  private:
   MyObject();
   ~MyObject();
-
   static NAN_METHOD(New);
   static NAN_METHOD(CallEmit);
   static Persistent<v8::Function> constructor;
+  void MyObject::ConnectToDamnVIsa();
+  
 };
 
 Persistent<v8::Function> MyObject::constructor;
@@ -56,3 +72,59 @@ NAN_METHOD(MyObject::CallEmit) {
 }
 
 NODE_MODULE(makecallback, MyObject::Init)
+
+
+
+/*********************************/
+
+void MyObject::ConnectToDamnVIsa()
+{
+  ViSession defaultRM;
+	ViSession instr1;
+	ViStatus status;
+	ViBuf bufferHandle;
+	ViEventType etype;
+	ViEvent eventContext;
+  
+  status = viOpenDefaultRM(&defaultRM);
+  status = viOpen(defaultRM, "GPIB0::11::INSTR", VI_NULL, VI_NULL, &instr1);
+  viGpibControlREN(instr1, VI_GPIB_REN_ASSERT);
+	write(instr1, "X");
+	viClear(instr1);
+	write(instr1, "M1X"); // enable SRQ on various errors
+
+	status = viInstallHandler(instr1, VI_EVENT_SERVICE_REQ, callback, bufferHandle);
+	status = viEnableEvent(instr1, VI_EVENT_SERVICE_REQ, VI_HNDLR, VI_NULL);
+  
+}
+
+
+ViStatus _VI_FUNCH callback(ViSession vi, ViEventType etype, ViEvent eventContext, ViAddr userHandle)
+{
+  v8::Handle<v8::Object> globalObj = Nan::GetCurrentContext()->Global();
+  
+	ViJobId jobID;
+	ViStatus status;
+	ViUInt16 stb;
+	status = viReadSTB(vi, &stb);
+	if ((status >= VI_SUCCESS) && (stb & 0x40))
+	{
+    
+    v8::Local<v8::Value> argv[1] = {
+      Nan::New("event").ToLocalChecked(),  // event name
+    };
+    MakeCallback(Nan::New<v8::Object>(this->This), "emit", 1, argv);
+		// printf("SQR :0x%02x\n", stb); /// yes, it's mine :-) 
+	}
+	status = viReadAsync(vi, (ViBuf)userHandle, MAX_CNT, &jobID);
+	return VI_SUCCESS;
+}
+
+
+ViStatus write(ViSession instr1, const char* input)
+{
+	ViUInt32 writeCount;
+	char temp[100];
+	_snprintf_s(temp, sizeof(temp), input);
+	return viWrite(instr1, (ViBuf)temp, (ViUInt32)strlen(temp), &writeCount);
+}
